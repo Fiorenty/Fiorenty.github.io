8faa988e60fd5d8ebb242083aa68819fa4feb7f8
Quinten <quinten.geeraerts@student.ucll.be>
2023-05-05 19:52:59 +0200

    New version 1

---
+++ Includers/JsIncluder/includenav.js
@@ -0,0 +1,9 @@
+function myMenu(){
+    if(document.getElementById("main_nav").style.display == "none" || document.getElementById("main_nav").style.display == "" ){
+        document.getElementById("main_nav").style.display = "block";
+    } else {
+        document.getElementById("main_nav").style.display = "none";
+    }
+}
+
+myMenu();
